138bdc0748f712f7348b55a65affcd343f009f50
Devin Ragotzy <devin.ragotzy@gmail.com>
2021-07-01 21:38:02 -0400

    Add create issue and sort of emoji verification

--- src/cmd_request.rs
+++ src/cmd_request.rs
@@ -21,6 +21,7 @@ use tokio::{
 #[derive(Clone, Debug)]
 pub enum Command {
     Octocat(String),
+    Create(String, Option<String>),
 }
 
 #[allow(unused)]
@@ -35,21 +36,29 @@ impl GithubBot {
     }
 
     async fn handle_incoming_message(&self, room: Joined, msg: &str) {
-        if msg.starts_with("!github") {
-            match msg.split_whitespace().skip(1).collect::<Vec<_>>().as_slice() {
-                [""] => {}
+        println!("{}", msg);
+        let send = |cmd| async {
+            self.sender.send((room.room_id().clone(), cmd)).await.unwrap()
+        };
+        if let Some(msg) = msg.strip_prefix("!github ") {
+            let cmds = split_cmd_words(msg).unwrap_or_default();
+            println!("{:?}", cmds);
+
+            match cmds.iter().map(|s| s.as_str()).collect::<Vec<&str>>().as_slice() {
+                ["party", msg] => send(Command::Octocat((*msg).to_owned())).await,
+                ["create", "issue", title] => {
+                    send(Command::Create((*title).to_owned(), None)).await
+                }
+                ["create", "issue", title, body] => {
+                    send(Command::Create((*title).to_owned(), Some((*body).to_owned())))
+                        .await
+                }
                 [] | [..] => {}
             }
-            self.sender
-                .send((
-                    room.room_id().clone(),
-                    Command::Octocat(msg.trim_start_matches("!party ").to_owned()),
-                ))
-                .await
-                .unwrap();
+
             println!("message sent");
         } else {
-            // TODO: check for exansions etc.
+            // TODO: check for expansions etc.
         }
     }
 }
@@ -81,6 +90,10 @@ impl EventHandler for GithubBot {
             };
             self.handle_incoming_message(room, msg_body).await;
         }
+    }
+
+    async fn on_custom_event(&self, _: Room, msg: &matrix_sdk::CustomEvent<'_>) {
+        println!("{:?}", msg)
     }
 
     /// This bot will auto join any room it is invited to.
@@ -139,4 +152,144 @@ pub async fn octocat_request(
     ));
     matrix.room_send(room, content, None).await?;
     Ok(())
+}
+
+pub async fn issue_request(
+    github: &GithubClient,
+    matrix: &MatrixClient,
+    room: &RoomId,
+    title: &str,
+    body: Option<&str>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    github
+        .send_github_request(rest::issue::create_issue::Request {
+            accept: None,
+            owner: "DevinR528",
+            repo: "magit",
+            title,
+            body,
+            labels: vec![],
+            assignees: vec![],
+            milestone: None,
+        })
+        .await?;
+    let content = AnyMessageEventContent::RoomMessage(MessageEventContent::new(
+        MessageType::Text(TextMessageEventContent::plain("Filed issue")),
+    ));
+    matrix.room_send(room, content, None).await?;
+    Ok(())
+}
+
+fn split_cmd_words(cmd: &str) -> Result<Vec<String>, &'static str> {
+    let mut out = vec![];
+    let mut buff = String::new();
+
+    let mut cmd = cmd.chars();
+
+    let mut in_quotes = false;
+    let mut quote_kind = None;
+    while let Some(c) = cmd.next() {
+        if c.is_whitespace() && !in_quotes {
+            out.push(buff.to_owned());
+            buff.clear();
+            continue;
+        }
+
+        // are these a problem ?
+        // `‘` | `’` | `“` | `”`
+        match c {
+            q @ '\'' | q @ '"' => {
+                if in_quotes && quote_kind == Some(q) {
+                    in_quotes = false;
+                    quote_kind = None;
+                } else if in_quotes && (quote_kind.is_none() || quote_kind != Some(q)) {
+                    buff.push(q)
+                } else {
+                    in_quotes = true;
+                    quote_kind = Some(q);
+                }
+            }
+            '\\' => match cmd.next() {
+                Some(c2) => buff.push(match c2 {
+                    'a' => '\u{07}',
+                    'b' => '\u{08}',
+                    'v' => '\u{0B}',
+                    'f' => '\u{0C}',
+                    'n' => '\n',
+                    'r' => '\r',
+                    't' => '\t',
+                    'e' | 'E' => '\u{1B}',
+                    '\\' => '\\',
+                    '\'' => '\'',
+                    '"' => '"',
+                    '$' => '$',
+                    '`' => '`',
+                    ' ' => ' ',
+                    _ => return Err("Invalid escape char"),
+                }),
+                None => return Err("Invalid escape char"),
+            },
+            c => buff.push(c),
+        }
+    }
+
+    if in_quotes {
+        return Err("Mismatched quotes oops");
+    }
+
+    if !buff.is_empty() {
+        out.push(buff);
+    }
+
+    Ok(out)
+}
+
+#[test]
+fn nothing_special() {
+    assert_eq!(split_cmd_words("a b c d").unwrap(), ["a", "b", "c", "d"]);
+}
+
+#[test]
+fn quoted_strings() {
+    assert_eq!(split_cmd_words("a \"b b\" a").unwrap(), ["a", "b b", "a"]);
+}
+
+#[test]
+fn escaped_double_quotes() {
+    assert_eq!(split_cmd_words("a \"\\\"b\\\" c\" d").unwrap(), ["a", "\"b\" c", "d"]);
+}
+
+#[test]
+fn escaped_single_quotes() {
+    assert_eq!(split_cmd_words("a \"'b' c\" d").unwrap(), ["a", "'b' c", "d"]);
+}
+
+#[test]
+fn escaped_spaces() {
+    assert_eq!(split_cmd_words("a b\\ c d").unwrap(), ["a", "b c", "d"]);
+}
+
+#[test]
+fn bad_double_quotes() {
+    assert_eq!(split_cmd_words("a \"b c d e").unwrap_err(), "Mismatched quotes oops");
+}
+
+#[test]
+fn bad_single_quotes() {
+    assert_eq!(split_cmd_words("a 'b c d e").unwrap_err(), "Mismatched quotes oops");
+}
+
+#[test]
+fn bad_quotes() {
+    assert_eq!(split_cmd_words("one '\"\"\"").unwrap_err(), "Mismatched quotes oops");
+}
+
+#[test]
+fn trailing_whitespace() {
+    assert_eq!(split_cmd_words("a b c d ").unwrap(), ["a", "b", "c", "d"]);
+}
+
+#[test]
+fn percent_signs() {
+    assert_eq!(split_cmd_words("abc '%foo bar%'").unwrap(), ["abc", "%foo bar%"]);
 }
